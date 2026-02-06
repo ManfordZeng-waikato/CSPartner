ee206b9c8b3c6edfacc4e86965b1573bf5030a35
ManfordZeng-waikato <zengchang389@gmail.com>
2026-02-06 18:33:23 +1300

    Add unit tests for comment deletion and creation in VideosControllerTests

--- Tests/API.Tests/Comments/CommentsControllerTests.cs
+++ Tests/API.Tests/Comments/CommentsControllerTests.cs
@@ -57,4 +57,51 @@ public class CommentsControllerTests : IClassFixture<CustomWebApplicationFactory
             updated!.IsDeleted.Should().BeTrue();
         }
     }
+
+    [Fact]
+    public async Task DeleteComment_returns_forbidden_when_not_owner()
+    {
+        Guid commentId;
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+
+            var otherUserId = Guid.NewGuid();
+            var video = new HighlightVideo(otherUserId, "t", "url");
+            db.Videos.Add(video);
+
+            var comment = new Comment(video.VideoId, otherUserId, "hello");
+            db.Comments.Add(comment);
+            await db.SaveChangesAsync();
+
+            commentId = comment.CommentId;
+        }
+
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Test-Auth", "true");
+
+        var response = await client.DeleteAsync($"/api/comments/{commentId}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task DeleteComment_returns_not_found_when_missing()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+        }
+
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Test-Auth", "true");
+
+        var response = await client.DeleteAsync($"/api/comments/{Guid.NewGuid()}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
 }
--- Tests/API.Tests/Videos/VideosControllerTests.cs
+++ Tests/API.Tests/Videos/VideosControllerTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using API.Tests.Helpers;
 using Application.DTOs.Common;
+using Application.DTOs.Comment;
 using Application.DTOs.Video;
 using Domain.Videos;
 using FluentAssertions;
@@ -134,5 +135,144 @@ public class VideosControllerTests : IClassFixture<CustomWebApplicationFactory>
         });
 
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task CreateComment_returns_created_and_persists_comment()
+    {
+        Guid videoId;
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+
+            var video = new HighlightVideo(TestAuthDefaults.UserId, "t", "url");
+            db.Videos.Add(video);
+            await db.SaveChangesAsync();
+            videoId = video.VideoId;
+        }
+
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Test-Auth", "true");
+
+        var response = await client.PostAsJsonAsync($"/api/videos/{videoId}/comments", new CreateCommentDto
+        {
+            Content = "hello"
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        var dto = await response.Content.ReadFromJsonAsync<CommentDto>(TestJsonOptions.Default);
+        dto.Should().NotBeNull();
+        dto!.Content.Should().Be("hello");
+
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            (await db.Comments.CountAsync()).Should().Be(1);
+        }
+    }
+
+    [Fact]
+    public async Task CreateComment_returns_unauthorized_when_not_authenticated()
+    {
+        Guid videoId;
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+
+            var video = new HighlightVideo(TestAuthDefaults.UserId, "t", "url");
+            db.Videos.Add(video);
+            await db.SaveChangesAsync();
+            videoId = video.VideoId;
+        }
+
+        var client = _factory.CreateClient();
+
+        var response = await client.PostAsJsonAsync($"/api/videos/{videoId}/comments", new CreateCommentDto
+        {
+            Content = "hello"
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task CreateComment_returns_bad_request_when_video_missing()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+        }
+
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Test-Auth", "true");
+
+        var response = await client.PostAsJsonAsync($"/api/videos/{Guid.NewGuid()}/comments", new CreateCommentDto
+        {
+            Content = "hello"
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task CreateComment_returns_bad_request_when_parent_missing()
+    {
+        Guid videoId;
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+
+            var video = new HighlightVideo(TestAuthDefaults.UserId, "t", "url");
+            db.Videos.Add(video);
+            await db.SaveChangesAsync();
+            videoId = video.VideoId;
+        }
+
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Add("X-Test-Auth", "true");
+
+        var response = await client.PostAsJsonAsync($"/api/videos/{videoId}/comments", new CreateCommentDto
+        {
+            Content = "hello",
+            ParentCommentId = Guid.NewGuid()
+        });
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetVideoComments_returns_list()
+    {
+        Guid videoId;
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            await db.Database.EnsureDeletedAsync();
+            await db.Database.EnsureCreatedAsync();
+
+            var video = new HighlightVideo(TestAuthDefaults.UserId, "t", "url");
+            db.Videos.Add(video);
+            await db.SaveChangesAsync();
+            videoId = video.VideoId;
+
+            db.Comments.Add(new Domain.Comments.Comment(videoId, TestAuthDefaults.UserId, "hello"));
+            await db.SaveChangesAsync();
+        }
+
+        var client = _factory.CreateClient();
+
+        var result = await client.GetFromJsonAsync<List<CommentDto>>($"/api/videos/{videoId}/comments", TestJsonOptions.Default);
+
+        result.Should().NotBeNull();
+        result!.Should().HaveCount(1);
+        result[0].Content.Should().Be("hello");
     }
 }
